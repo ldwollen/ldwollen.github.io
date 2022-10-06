935e18f2f52420b89b31a26bd8a062bac639db3b
Luke Wollenweber <112636379+ldwollen@users.noreply.github.com>
2022-10-06 10:56:16 -0400

    Update assignment03.js

--- assignment03.js
+++ assignment03.js
@@ -1,6 +1,13 @@
-function convert() {
-  let fahrenheit = document.getElementById("F_input").value;
-  let convertedData = (fahrenheit - 32) * (5/9);
-  document.getElementById("C_output").value = convertedData;
+// function convert() {
+//   let fahrenheit = document.getElementById("F_input").value;
+//   let convertedData = (fahrenheit - 32) * (5/9);
+//   document.getElementById("C_output").value = convertedData;
   
-}
+// }
+
+var addButton = document.querySelector("#convert")
+addButton.addEventListener("click", e => {
+  let inputObject = document.getElementById("F_input")
+  let num = (parseFloat(inputObject.value) -32) * (5/9)
+  C_output.value = num
+})
