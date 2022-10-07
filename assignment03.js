7c4a9fe470cd53b0b9a3be52fc92ab192b1a2f8a
Luke Wollenweber <112636379+ldwollen@users.noreply.github.com>
2022-10-06 21:00:50 -0400

    Update assignment03.js

--- assignment03.js
+++ assignment03.js
@@ -1,17 +1,3 @@
-// function convert() {
-//   let fahrenheit = document.getElementById("F_input").value;
-//   let convertedData = (fahrenheit - 32) * (5/9);
-//   document.getElementById("C_output").value = convertedData;
-  
-// }
-
-var addButton = document.querySelector("#convert")
-addButton.addEventListener("click", e => {
-  let inputObject = document.getElementById("F_input")
-  let num = (parseFloat(inputObject.value) -32) * (5/9)
-  C_output.value = num
-})
-
 function calculateGCD() {
   let firstNum = document.getElementById("first_number").value;
   let secondNum = document.getElementById("second_number").value;
