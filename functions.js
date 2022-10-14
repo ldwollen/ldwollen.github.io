f0328ec72ac9d29cd3147045300b81108f1004e8
Luke Wollenweber <112636379+ldwollen@users.noreply.github.com>
2022-10-14 17:39:28 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,43 +1,39 @@
+//This file contains code that fills the middle rows of the art table, some calculated and some pulled from an array. This code was based on what we went over in class, following Logan's code and designs
+//The first function is for calculating a price, and the second fills the rows of the table by pulling values from the js array, and calcuaitng tax and totals based off of the user's input in the alert box
 function calculateTotal(quantity, price) {
     return quantity * price;
 }
 
 function outputCartRow(item, total) {
-  // https://loganzipper.github.io/Assignments/assignment03/js/functions.js
 
-  let tax = prompt("What is the tax?", "0.10");
-  let shipMinimum = prompt("What is the shipping threshold?", "300");
+  let tax = prompt("Please enter the tax?", "0.10");
+  let shipMinimum = prompt("Please enter the shipping threshold?", "300");
 
-  // select the HTML table
-  //Save for later
   var subtotal = 0;
-  let tbl = document.getElementsByClassName("art_Table")[0];
+  let table = document.getElementsByClassName("art_Table")[0];
 
   let i = 0;
   while (i < cart.length) {
-    let row = tbl.insertRow(i + 1); // skip first row (column headings)
+    let row = table.insertRow(i + 1); 
 
-    // create an empty cell for each column to appear in HTML table
-    let cell0 = row.insertCell(0); // image
-    let cell1 = row.insertCell(1); // product
-    let cell2 = row.insertCell(2); // quantity
-    let cell3 = row.insertCell(3); // price
-    let cell4 = row.insertCell(4); // totalAmount
+    let cell0 = row.insertCell(0); 
+    let cell1 = row.insertCell(1); 
+    let cell2 = row.insertCell(2); 
+    let cell3 = row.insertCell(3); 
+    let cell4 = row.insertCell(4); 
 
     let quantity = cart[i].quantity;
     let price = cart[i].product.price.toFixed(2);
     let curTotal = calculateTotal(quantity, price).toFixed(2);
     subtotal += parseFloat(curTotal);
 
-    // populate HTML table with data
-    cell0.innerHTML = "<img src = '../" + cart[i].product.filename + "'>"; // image
-    cell1.innerHTML = cart[i].product.title; // product
-    cell2.innerHTML = quantity; // quantity
-    cell3.innerHTML = "$" + price; // price
-    cell4.innerHTML = "$" + curTotal; // total
+    cell0.innerHTML = "<img src = '../" + cart[i].product.filename + "'>"; 
+    cell1.innerHTML = cart[i].product.title; 
+    cell2.innerHTML = quantity; 
+    cell3.innerHTML = "$" + price; 
+    cell4.innerHTML = "$" + curTotal; 
     i++;
 
-    //Align Text and adjust image table cell
     cell0.style.width = "140px";
     cell2.style.textAlign = "center";
     cell3.style.textAlign = "center";
@@ -46,7 +42,6 @@ function outputCartRow(item, total) {
 
   let shipping = subtotal < shipMinimum ? 40 : 0;
 
-  //After page load, update bottom of table with correct total amounts
   let totals = document.getElementsByClassName("totals");
   totals[0].getElementsByTagName("td")[1].innerHTML = "$" + subtotal.toFixed(2);
   totals[1].getElementsByTagName("td")[1].innerHTML =
